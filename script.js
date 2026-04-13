f44f8084316f6fb11af2a91e1e6bb69fac2f4fee
Hackarea <shahinashaji572@gmail.com>
2026-04-13 13:35:55 +0530

    portfolio upgrade

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Cybersecurity Student | Ethical Hacker";
+let i = 0;
+
+function typing() {
+    if (i < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+
+typing();
