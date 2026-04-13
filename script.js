6952dbab626b2d2aa47200e7fef5732cc8cad7a6
Hackarea <shahinashaji572@gmail.com>
2026-04-13 13:50:41 +0530

    full professional portfolio

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let i = 0;
 
 function typing() {
     if (i < text.length) {
-        document.querySelector(".typing").innerHTML += text.charAt(i);
+        document.getElementById("typing").innerHTML += text.charAt(i);
         i++;
         setTimeout(typing, 50);
     }
